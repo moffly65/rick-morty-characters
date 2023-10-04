3f693d5c982546d34e34fc010c980e59f15d2e40
moffly65 <moffly65@gmail.com>
2023-10-04 17:51:38 +0200

    Initial Commit

---
+++ Rick and Morty/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Rick and Morty
+//
+//  Created by Jose Maria Toro on 4/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rick and Morty/Rick_and_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_and_MortyApp.swift
+//  Rick and Morty
+//
+//  Created by Jose Maria Toro on 4/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Rick_and_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
